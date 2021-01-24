aebbe27f372b3de34897d31f1a3b4b8185ad0e44
Igor Talankin <italankin@gmail.com>
2021-01-24 13:36:04 +0500

    Added type hints for config

--- src/config.py
+++ src/config.py
@@ -3,6 +3,12 @@ from enum import Enum
 
 
 class Config:
+    bot_token: str
+    inline_debounce_millis: int
+    min_message_length: int
+    max_message_length: int
+    output_format: dict
+
     def __init__(self):
         self.bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
         self.inline_debounce_millis = int(os.getenv('TELEGRAM_INLINE_DEBOUNCE_MILLIS', "1000"))
@@ -16,6 +22,11 @@ class Config:
         self.aws = Config.Aws()
 
     class Aws:
+        access_key_id: str
+        secret_access_key: str
+        region_name: str
+        s3_bucket: str
+
         def __init__(self):
             self.access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
             self.secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
